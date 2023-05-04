43cb882c009b51d5c3ffd4a38d75220d5cbe9179
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-05-04 19:59:50 +0600

    done ema john

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
     // console.log(props.product)
@@ -15,7 +17,9 @@ const Product = (props) => {
                 <p>Seller:{seller}</p>
                 <p>Ratings:{ratings} stars</p>
             </div>
-            <button onClick={() => handleaddtoCart(props.product)} className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleaddtoCart(props.product)} className='btn-cart'>Add to Cart<FontAwesomeIcon icon={faShoppingCart} />
+
+            </button>
         </div>
     );
 };
